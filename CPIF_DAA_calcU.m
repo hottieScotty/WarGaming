5defb06beb1d9b3f153be776ab9adda771a72e46
bmdyer1 <bdyer17@ymail.com>
2017-06-08 16:18:15 -0400

    Update CPIF_DAA_calcU.m

--- CPIF_DAA_calcU.m
+++ CPIF_DAA_calcU.m
@@ -1,7 +1,11 @@
-function [SR,Tp,GovPay] = CPIF_DAA_calcU(alpha,Ac,Oc,Tc,Cp)
+function [SR,Tp,GovPay] = CPIF_DAA_calcU(alpha,Ac,Tc,Cp)
 % UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
+SR=2*(2-alpha)*(1/3)
+
+Tp = Cp - (3/2)*SR*(Tc - Ac)
+
+GovPay = Ac + (Tp - Tc) + SR*(Tc - Ac)
 
 end
-

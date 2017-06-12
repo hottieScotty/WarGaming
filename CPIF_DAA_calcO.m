008596db0c7f066c234daa5442e0a2239d59d0cc
hottieScotty <samahan@asu.edu>
2017-06-12 13:51:11 -0400

    Add files via upload

--- CPIF_DAA_calcO.m
+++ CPIF_DAA_calcO.m
@@ -1,4 +1,4 @@
-function [SR,Tp,GovPay] = CPIF_DAA_calcO(beta,Ac,Tc,Cp,i,n_opt)
+function [SR,Tp,GovPay,profit,GovS] = CPIF_DAA_calcO(beta,Ac,Tc,Cp,i,n_opt)
 % solves for some optimal CPIF contract parameters
 % called in CPIF_DAA_func.m
 
@@ -20,5 +20,27 @@ Tp = Cp - 1.5*SR*(Tc - Ac);
 
 % compute government payment
 GovPay = Ac + (Tp - Tc) + SR*(Tc - Ac);
+
+%{
+%compute government savings and profit
+PGO = -Cp; % from slides, note as different than above case
+    PKO = Cp-Ac;
+    PGF = @(s) Ac + (Tp - Tc) - s*(Ac - Tc); % each flips ac tc and uses overun share
+    PKF = @(s) (Tp - Tc)+(1-s)*(Ac - Tc);
+    PK = @(s) (1-s)*(Ac - Tc);
+
+PG = @(s) 2*(1-s)*(Tc-Ac)-Tp; %one payoff gov function for optimization
+
+toMin = @(s) -(PG(s)-PGO)*(PK(s)-PKO); %minimizes negative of optimzation function from slides
+
+sRO = fminbnd(toMin, 0, 1);%matlab function which minimizes
+
+PCFG = PGF(sRO); %returns final payoff based on final payoff functions
+%}
+
+profit = (Tp - Tc) + (1-SR)*(Ac - Tc);
+
+GovS = Cp+(1-SR)*(Tc-Ac) - GovPay;
+
 
 end
--- CPIF_DAA_calcU.m
+++ CPIF_DAA_calcU.m
@@ -1,4 +1,4 @@
-function [SR,Tp,GovPay] = CPIF_DAA_calcU(alpha,Ac,Tc,Cp,i,n_opt)
+function [SR,Tp,GovPay,profit,GovS] = CPIF_DAA_calcU(alpha,Ac,Tc,Cp,i,n_opt)
 % solves for some optimal CPIF contract parameters
 % called in CPIF_DAA_func.m
 
@@ -20,5 +20,26 @@ Tp = Cp - 1.5*SR*(Tc - Ac);
 
 % compute government payment
 GovPay = Ac + (Tp - Tc) + SR*(Tc - Ac);
+
+%{
+%compute Profit and Government Savings
+    PGO = (2*(Tc-Ac) - Cp); %benchmark payoff gov and payoff ktr, from slides
+    PKO = (Tp-Tc);
+    PGF = @(s) Ac + (Tp - Tc) + s*(Tc - Ac); %final (positive) payoff gov function
+    PKF = @(s) (Tp - Tc)+(s)*(Tc - Ac); %final payoff function ktr
+    PK = @(s) (s)*(Tc - Ac); %payoff function for maximzation
+
+PG = @(s) 2*(1-s)*(Tc-Ac)-Tp; %one payoff gov function for optimization
+
+toMin = @(s) -(PG(s)-PGO)*(PK(s)-PKO); %minimizes negative of optimzation function from slides
+
+sRO = fminbnd(toMin, 0, 1); %matlab function which minimizes
+
+PCFG = PGF(sRO); %returns final payoff based on final payoff functions
+%}
+
+profit = (Tp - Tc)+SR*(Tc - Ac);
+
+GovS = Cp+(1-SR)*(Tc-Ac)-GovPay;
 
 end
